df2bc8257d4fe3fb91dd6616fcad05cfebaa1bae
rrramdhani <106809140+rrramdhani@users.noreply.github.com>
2022-06-03 16:43:36 +0700

    Add files via upload

---
+++ neuron/03/casting.php
@@ -0,0 +1,16 @@
+<?php
+$var1           = 19;
+$var2           = "Jumlah peserta 18";
+$nilai          = 18.5;
+$boolean        = true;
+
+echo (int)$nilai; /* Casting float to integer */
+echo '<br>---------------------------------------<br>';
+
+echo (double)$var1; /* Casting integer to decimal */
+echo '<br>---------------------------------------<br>';
+
+echo (int)$var2; /* Casting string to int */
+echo '<br>---------------------------------------<br>';
+
+echo $boolean;
---
+++ neuron/03/checkVariable.php
@@ -0,0 +1,8 @@
+<?php
+$var1 = 10;
+if(isset($var1)){
+    echo "true";
+}else{
+    echo "false";
+}
+
